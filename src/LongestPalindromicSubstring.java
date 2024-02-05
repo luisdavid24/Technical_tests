159c40c10776a4e78f45eaac9aaff26e4803ae13
Luis David <david12.29@hotmail.com>
2024-02-05 11:24:31 -0500

    Update LongestPalindromicSubstring class

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -4,3 +4,39 @@ public class LongestPalindromicSubstring {
         System.out.println("Hello world!");
     }
 }
+
+
+
+class SolLongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+
+        return s;
+    }
+}
+/*
+
+
+Given a string s, return the longest
+palindromic
+
+substring
+ in s.
+
+
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+
+
+Constraints:
+
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+ */
