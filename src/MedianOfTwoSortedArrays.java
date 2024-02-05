8a31ef2e4c15edef9ae5555b83aa6d35f734b72c
Luis David <david12.29@hotmail.com>
2024-02-04 23:00:36 -0500

    Update Media two sorted arrays

--- src/MedianOfTwoSortedArrays.java
+++ src/MedianOfTwoSortedArrays.java
@@ -3,33 +3,41 @@ public class MedianOfTwoSortedArrays {
     public static void main(String[] args) {
         SolMedianTwoSortedArrays solutionObject= new SolMedianTwoSortedArrays();
         int[] vector={1,3};
-        int[] vector2={2};
+        int[] vector2={2,7};
         System.out.println(solutionObject.findMedianSortedArrays(vector,vector2));
 
     }
-
-
-
-
-
-
-
 }
 
-
 class SolMedianTwoSortedArrays {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n= nums1.length+ nums2.length;
-        int sum=0;
-        for(int e:nums1){
-            sum+=e;
+    private int p1 = 0, p2 = 0;
+    private int getMin(int[] nums1, int[] nums2) {
+        if (p1 < nums1.length && p2 < nums2.length) {
+            return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
+        } else if (p1 < nums1.length) {
+            return nums1[p1++];
+        } else if (p2 < nums2.length) {
+            return nums2[p2++];
         }
-        for(int e:nums2){
-            sum+=e;
-        }
-
-        return (double)sum/n;
+        return -1;
     }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        if ((m + n) % 2 == 0) {
+            for (int i = 0; i < (m + n) / 2 - 1; ++i) {
+                int tmp = getMin(nums1, nums2);
+                
+            }
+            return (double)(getMin(nums1, nums2) + getMin(nums1, nums2)) / 2;
+        } else {
+            for (int i = 0; i < (m + n) / 2; ++i) {
+                int tmp = getMin(nums1, nums2);
+                System.out.println("tmp: "+tmp);
+            }
+            return getMin(nums1, nums2);
+        }
+    }
+
 }
 
 
