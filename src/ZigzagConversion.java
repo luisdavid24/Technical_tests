cc9fe03a3eede98e684f0cc4d3ad58edff5a2901
Luis David <david12.29@hotmail.com>
2024-02-06 23:09:50 -0500

    Update  Zigzag conversion it is no workking

--- src/ZigzagConversion.java
+++ src/ZigzagConversion.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 public class ZigzagConversion {
 
     public static void main(String[] args) {
 
-        
+        SolutionZigzagConversion element= new SolutionZigzagConversion();
+        String variable="PAYPALISHIRING";
+        int rows=4;
+        System.out.println(element.convert(variable,rows));
 
     }
 
@@ -11,14 +16,68 @@ public class ZigzagConversion {
 
 class SolutionZigzagConversion {
     public String convert(String s, int numRows) {
+        int columns=s.length();
+        String[][] matrix= new String[numRows][columns];
 
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j <  columns; j++) {
+                    matrix[i][j] = "*";
+            }
+        }
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println("          //////        "+columns);
+        int k=0,aux=1,posColumna=0,posfila=0;
+        while(k<12){
+            for(int i=0;i<numRows;i++){
+                matrix[posfila][posColumna] = "k";
+                matrix[posfila][posColumna] = s.substring(k,aux);
+                posfila++;
+                k++;
+                aux++;
+
+            }
+            posfila--;
+            for(int i=0;i<numRows-2;i++){
+                posfila--;
+                posColumna++;
+                matrix[posfila][posColumna] = s.substring(k,aux);
+                k++;
+                aux++;
+            }
+            posColumna++;
+            posfila--;
+
+        }
+
+
+
+        //matrix[0][0] = s.substring(1,2);
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+        return null;
     }
 }
 
 
 
 
-/*The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+/*The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+(you may want to display this pattern in a fixed font for better legibility)
 
 P   A   H   N
 A P L S I I G
