3efe019ce1dce9ea36171a911fd79da0ee063821
Luis David <david12.29@hotmail.com>
2024-02-13 14:35:40 -0500

    Update Palindrome Number class

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,2 +1,23 @@
 public class PalindromeNumber {
 }
+
+class SolutionPalindromeNumber {
+    public boolean isPalindrome(int x) {
+        return false;
+    }
+}
+
+
+/*
+Given an integer x, return true if x is a
+palindrome
+, and false otherwise.
+
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+ */
