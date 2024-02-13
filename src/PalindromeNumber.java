5f62f0f9c2a0e0c3b5e8021da057b3bb187fc14e
Luis David <david12.29@hotmail.com>
2024-02-13 14:54:37 -0500

    Update Palindrome Number class is working

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -2,18 +2,37 @@ public class PalindromeNumber {
 
     public static void main(String[] args) {
         SolutionPalindromeNumber element= new SolutionPalindromeNumber();
-        int number=12345;
+        int number=1;
         System.out.println( element.isPalindrome(number));
     }
 }
 
 class SolutionPalindromeNumber {
     public boolean isPalindrome(int x) {
-        String numberString = Integer.toString(x);
+        if (x < 0)
+            return false;
+
+        int reversed = 0;
+        int original = x;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+
+    }
+}
+
+
+
+/*
+String numberString = Integer.toString(x);
         char[] vector=numberString.toCharArray();
         int i=0,j=vector.length-1;
         while(i<=vector.length/2 && j>=vector.length/2){
-            System.out.println(vector[i]+" "+vector[j]);
             if(vector[i] != vector[j]){
                 return false;
             }
@@ -23,12 +42,11 @@ class SolutionPalindromeNumber {
 
 
 
+
+
         return true;
-    }
-}
 
 
-/*
 Given an integer x, return true if x is a
 palindrome
 , and false otherwise.
