b8fc548a927cf7b390eedae2d27288f846357277
Luis David <david12.29@hotmail.com>
2024-02-13 14:47:14 -0500

    Update Palindrome Number class is going to be working

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,9 +1,29 @@
 public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        SolutionPalindromeNumber element= new SolutionPalindromeNumber();
+        int number=12345;
+        System.out.println( element.isPalindrome(number));
+    }
 }
 
 class SolutionPalindromeNumber {
     public boolean isPalindrome(int x) {
-        return false;
+        String numberString = Integer.toString(x);
+        char[] vector=numberString.toCharArray();
+        int i=0,j=vector.length-1;
+        while(i<=vector.length/2 && j>=vector.length/2){
+            System.out.println(vector[i]+" "+vector[j]);
+            if(vector[i] != vector[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+
+
+        return true;
     }
 }
 
