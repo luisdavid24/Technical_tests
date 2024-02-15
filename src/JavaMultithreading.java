a39569152536fee8e0c23e4e09e4bdf7b65576a0
Luis David <david12.29@hotmail.com>
2024-02-15 17:16:26 -0500

    Update Java Multithreading class

--- src/JavaMultithreading.java
+++ src/JavaMultithreading.java
@@ -1,2 +1,41 @@
 public class JavaMultithreading {
+
+    public static void main(String[] args) {
+
+    }
 }
+
+class Corredor implements  Runnable{
+    private String nombre;
+    private int distanciaTotal;
+    private int distanciaRecorrida;
+
+    public Corredor(String nombre, int distanciaTotal) {
+        this.nombre = nombre;
+        this.distanciaTotal = distanciaTotal;
+        this.distanciaRecorrida = 0;
+    }
+
+
+
+    @Override
+    public void run() {
+
+    }
+}
+
+
+/*
+
+
+Enunciado del ejercicio:
+Escribe un programa en Java que simule una carrera entre varios corredores. Cada corredor será representado por un hilo de ejecución. El programa deberá mostrar el progreso de la carrera en la consola y determinar el ganador.
+
+Requisitos:
+Debes crear una clase Corredor que implemente la interfaz Runnable. Cada corredor tendrá un nombre y una velocidad de carrera aleatoria.
+Los corredores deben avanzar de forma aleatoria en cada iteración de la carrera.
+El progreso de la carrera debe mostrarse en la consola. Puedes utilizar barras de progreso o simplemente imprimir mensajes indicando la posición de cada corredor en cada iteración.
+El primer corredor que alcance una distancia determinada (por ejemplo, 100 metros) será declarado como el ganador.
+El programa debe imprimir el nombre del corredor ganador al finalizar la carrera.
+
+ */
