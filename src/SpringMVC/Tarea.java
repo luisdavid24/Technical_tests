fe5331b082e31e60b2fce5a136914e10ddfd944c
Luis David <david12.29@hotmail.com>
2024-02-16 06:53:00 -0500

    Update Tarea class has constructor

--- src/SpringMVC/Tarea.java
+++ src/SpringMVC/Tarea.java
@@ -1,4 +1,48 @@
 package SpringMVC;
 
 public class Tarea {
+
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private boolean completada;
+
+    public Tarea(int id, String nombre, String descripcion, boolean completada) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.completada = completada;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
 }
