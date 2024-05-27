b0e700429e9d75c43a2837d378dc4aa48a9fa587
Luis David <david12.29@hotmail.com>
2024-05-27 11:09:06 -0500

    Refactor, sum All number now is working also it is perfect

--- src/sumAllNumber.java
+++ src/sumAllNumber.java
@@ -1,10 +1,11 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class sumAllNumber {
     
     
     public static void main(String[] args) {
-        int[] intArray = generateRandomInt();
+        int[] intArray = generateRandomIntArray(5,10);
         for(int element : intArray){
             System.out.println(element);
         }
@@ -12,20 +13,17 @@ public class sumAllNumber {
     }
 
     public static int sumAll(int[] array){
-        int total=0;
-        for(int element : array){
-            total+=element;
-        }
-        return total;
+        
+        return Arrays.stream(array).max().orElseThrow();
     }
-    public static int[] generateRandomInt(){
-        int[] array = new int[10];
+    public static int[] generateRandomIntArray(int size, int bound) {
+        int[] array = new int[size];
         Random random = new Random();
-        
+
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(100);
+            array[i] = random.nextInt(bound);
         }
-        
+
         return array;
     }
 }
