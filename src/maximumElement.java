6cab185ba9eac772c71237b64e4ed4b9d12a03a7
Luis David <david12.29@hotmail.com>
2024-05-27 10:10:36 -0500

    Refactor, maximum element is done

--- src/maximumElement.java
+++ src/maximumElement.java
@@ -3,26 +3,32 @@ import java.util.Random;
 public class maximumElement {
 
     public static void main(String[] args) {
-       float[] floatArray = new float[10];
-       Random random = new Random();
-        
-        for (int i = 0; i < floatArray.length; i++) {
-            floatArray[i] = random.nextFloat()*9;
-        }
+        float[] floatArray = generateRandomFloatArray(10);
+
         for (float num : floatArray) {
             System.out.println(num);
         }
-        max(floatArray);
+        float maxNumber = max(floatArray);
+        System.out.println("The maximum number is: " + maxNumber);
     }
 
-    public static void max(float[] array){
+    public static float max(float[] array){
         float max=array[0];
         for (float num : array) {
             if(num>max){
                 max=num;
             }
         }
-        System.out.println("This is max number");
-        System.out.println(max);
+        return max;
+    }
+    public static float[] generateRandomFloatArray(int size) {
+        float[] array = new float[size];
+        Random random = new Random();
+        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextFloat() * 9;
+        }
+        
+        return array;
     }
 }
