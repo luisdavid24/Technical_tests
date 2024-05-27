35a759e9295e6c816df08f5ba0a7a9cb0ac2330b
Luis David <david12.29@hotmail.com>
2024-05-27 10:23:49 -0500

    Refactor, world palindrome class was done by me

--- src/wordPalindrome.java
+++ src/wordPalindrome.java
@@ -1,6 +1,26 @@
 public class wordPalindrome {
  
     public static void main(String[] args) {
-       System.out.println("This was an ambush");
+       String world="anna";
+       if(palindrome(world)){
+            System.out.println(world+" is palindrome");
+       }else{
+        System.out.println(world+" is not palindrome");
+       }
+    }
+
+    public static boolean palindrome(String str){
+        char[] vector=str.toCharArray();
+        int left=0, right=vector.length-1;
+        while(left<right){
+            if(vector[left]==vector[right]){
+                left++;
+                right--;
+            }else{
+                return false;
+            }
+        }
+
+        return true;
     }
 }
