5286ee39e31de1d86e003ac1664e0e2f032ae2ac
Luis David <david12.29@hotmail.com>
2024-05-27 11:00:55 -0500

    Refactor, sum all numer is working but it is not perfect

--- src/sumAllNumber.java
+++ src/sumAllNumber.java
@@ -1,7 +1,31 @@
+import java.util.Random;
+
 public class sumAllNumber {
     
     
     public static void main(String[] args) {
-        System.out.println("I am king");
-     }
+        int[] intArray = generateRandomInt();
+        for(int element : intArray){
+            System.out.println(element);
+        }
+        System.out.println("Total is: "+sumAll(intArray));
+    }
+
+    public static int sumAll(int[] array){
+        int total=0;
+        for(int element : array){
+            total+=element;
+        }
+        return total;
+    }
+    public static int[] generateRandomInt(){
+        int[] array = new int[10];
+        Random random = new Random();
+        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        
+        return array;
+    }
 }
