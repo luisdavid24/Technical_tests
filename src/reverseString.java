cc9046c1bdf4495c834819be1a8316d87081fe2f
Luis David <david12.29@hotmail.com>
2024-05-27 08:47:25 -0500

    Refactor, reverseString has some change

--- src/reverseString.java
+++ src/reverseString.java
@@ -1,6 +1,12 @@
 public class reverseString {
     
     public static void main(String[] args) {
-        System.out.println("Hello reverseString!");
+        String cadena="ssss";
+        System.out.println(reverse(cadena));
+    }
+
+    private static String reverse(String str){
+        
+        return str;
     }
 }
