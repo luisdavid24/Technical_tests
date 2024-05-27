a8ef9b8531933635f6ed3e9d67e1ebd6b5fdd299
Luis David <david12.29@hotmail.com>
2024-05-27 09:58:49 -0500

    Refactor, maximunElement has some change but is not done

--- src/maximumElement.java
+++ src/maximumElement.java
@@ -1,8 +1,28 @@
+import java.util.Random;
+
 public class maximumElement {
 
-    
     public static void main(String[] args) {
-       System.out.println("Hello I am david");
+       float[] floatArray = new float[10];
+       Random random = new Random();
+        
+        for (int i = 0; i < floatArray.length; i++) {
+            floatArray[i] = random.nextFloat()*9;
+        }
+        for (float num : floatArray) {
+            System.out.println(num);
+        }
+        max(floatArray);
     }
 
+    public static void max(float[] array){
+        float max=array[0];
+        for (float num : array) {
+            if(num>max){
+                max=num;
+            }
+        }
+        System.out.println("This is max number");
+        System.out.println(max);
+    }
 }
