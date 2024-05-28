652f06a737de0f3200e07b68761eb7ba21826d70
Luis David <david12.29@hotmail.com>
2024-05-28 11:58:16 -0500

    Refactor, I did some change in pair number class but is not finish

--- src/pairNumbers.java
+++ src/pairNumbers.java
@@ -1,6 +1,28 @@
+import java.util.Random;
+
 public class pairNumbers {
  
     public static void main(String[] args) {
         System.out.println("Hello I am david");
+        int[] array=generateAnArray(15);
+        seeArray(array);
     }
+    public static int[] generateAnArray(int size){
+        int[] array = new int[size];
+        Random random = new Random();
+        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(21);
+        }
+        
+        return array;
+    }
+
+    public static void seeArray(int[] array){
+        for(int element:array){
+            System.out.print(element+" ");
+        }
+    }
+    
+
 }
