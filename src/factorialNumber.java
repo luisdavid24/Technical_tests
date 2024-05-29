d73243d864a0ed608d6722e43abadfbdc21d816a
Luis David <david12.29@hotmail.com>
2024-05-29 10:13:41 -0500

    Refactor, factorial number is working

--- src/factorialNumber.java
+++ src/factorialNumber.java
@@ -1,3 +1,19 @@
 public class factorialNumber {
     
+    public static void main(String[] args) {
+        System.out.println("This is the result: "+factorial(5));
+        
+    }
+
+    public static int factorial(int number){
+        int aux=1;
+        for(int i=number;i>=0;i--){
+            if(i==0){
+                return aux;
+            }
+            aux*=i;
+        }
+        return aux;
+    }
+    
 }
