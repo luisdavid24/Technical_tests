bc66de96d9f94a174c978cae1e5214bf192c63ac
Luis David <david12.29@hotmail.com>
2024-05-28 21:39:47 -0500

    Refactor, fibonacci is perfect

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -9,21 +9,20 @@ public class fibonacci {
     }
 
     public static int[] generateFibonacci(int size){
-        if(size>=3){
-            int[] aux=new int[size];
-            aux[0]=1;
-            aux[1]=1;
-            for(int i=2;i<size;i++){
-                aux[i]=aux[i-1]+aux[i-2];
-            }
-            return aux;
-        }else if(size>2){
-            int[] aux={1, 1};
-            return aux;
-        }else if(size==1){
-            int[] aux={1};
-            return aux;
+        if (size <= 0) {
+            return new int[0];
         }
-        return null;
+        int[] fibonacciArray = new int[size];
+        if (size > 0) {
+            fibonacciArray[0] = 1;
+        }
+        if (size > 1) {
+            fibonacciArray[1] = 1;
+        }
+        for (int i = 2; i < size; i++) {
+            fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+        }
+        
+        return fibonacciArray;
     }
 }
